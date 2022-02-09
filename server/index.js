35e65ecd9ba17ec2914af67fbe8b4e3a42b25c70
ghassenbenzahra123 <ghassen.benzahra@esprit.tn>
2022-02-09 04:49:04 +0100

    add send SMS feature

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const authRoutes = require('./routes/auth.js');
 
 const PORT = process.env.PORT || 5000;
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 require('dotenv').config();
 
 app.use(cors());
@@ -18,5 +22,29 @@ app.use('/auth', authRoutes);
 app.get('/', (req, res) => {
     res.send('Hello world!');
 })
+
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if (type === 'message.new') {
+        members
+            .filter((member) => member.user_id !== sender.id)
+            .forEach(({ user }) => {
+                if (!user.online) {
+                    twilioClient.messages.create({
+                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                        .then(() => console.log('Message sent!'))
+                        .catch((err) => console.log(err));
+                }
+            })
+
+        return res.status(200).send('Message sent!');
+    }
+
+    return res.status(200).send('Not a new message request');
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
